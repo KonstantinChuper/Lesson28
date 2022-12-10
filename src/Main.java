1a3e2e481635797b4937d95b1ae025e76c4c592e
KonstantinChuper <konstantyn.chuper@gmail.com>
2022-12-10 11:54:43 +0100

    Lesson 28

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main { public static void main(String[] args) {
+    //System.out.println("Hello to Me!");
+    /*int a=10;
+    double b=5.4;
+    System.out.println(a<b);
+    System.out.println(++b);
+    System.out.println(a++);
+    System.out.println(b--);
+    System.out.println(a);
+    System.out.println(b);*/
+    int age=17;
+    if (age>=18){
+    System.out.println("You are an adult person! Graz!");}
+    else {System.out.println("You are not an adult person!");}
+    int weekday = 1;
+    if(weekday==1) {
+        System.out.println("You are an adult person! Graz!");}
+
+
     }
 }
